a0900a9de881c6a63f4c946feee445708c3d9b12
Vanneka <vanneka.okeke@gmail.com>
2019-05-23 01:07:06 +0100

    Updates

---
+++ precache-manifest.1344e6e1327b81811fd8417422a5575c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "812195ec727a9047946c4d1ff3dd134b",
+    "url": "/val-memoirs/index.html"
+  },
+  {
+    "revision": "4e1cdb1b081231b64d9a",
+    "url": "/val-memoirs/static/css/2.d2e2e5fa.chunk.css"
+  },
+  {
+    "revision": "ec49ba4f1abfbda719f8",
+    "url": "/val-memoirs/static/css/main.541e984a.chunk.css"
+  },
+  {
+    "revision": "4e1cdb1b081231b64d9a",
+    "url": "/val-memoirs/static/js/2.bfe09438.chunk.js"
+  },
+  {
+    "revision": "ec49ba4f1abfbda719f8",
+    "url": "/val-memoirs/static/js/main.7ed12684.chunk.js"
+  },
+  {
+    "revision": "5255aed57ccc3c8ecd55",
+    "url": "/val-memoirs/static/js/runtime~main.6989fa7f.js"
+  }
+]);
